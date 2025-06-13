b4ee586f0e01f04faf9cbeafc385f323a7694f12
Campbell Kramer <campbellkramer@gmail.com>
2025-06-13 01:32:02 -0230

    Add Jest test file to test getStreak function

--- __tests__/utils/utilities.test.js
+++ __tests__/utils/utilities.test.js
@@ -1,5 +1,33 @@
-const { getStreak } = require('../utils/mood');
+const { getStreak } = require("../../utils/utilities");
 
-describe("Tests for getStreak", () => {
+describe("getStreak", () => {
+  test("returns full streak for all positive moods when list is entirely positive moods", () => {
+    const moods = ["Happy", "Excited", "Happy"];
+    expect(getStreak(moods)).toBe(3);
+  });
 
+  test("returns 0 if most recent mood is negative", () => {
+    const moods = ["Sad", "Happy", "Sad"];
+    expect(getStreak(moods)).toBe(0);
+  });
+
+  test("returns correct streak leading up to first negative mood", () => {
+    const moods = ["Happy", "Sad", "Excited", "Happy", "Excited"];
+    expect(getStreak(moods)).toBe(3);
+  });
+
+  test("returns 0 for array with no positive moods", () => {
+    const moods = ["Sad", "Okay", "Stressed"];
+    expect(getStreak(moods)).toBe(0);
+  });
+
+  test("returns 0 for empty mood array", () => {
+    const moods = [];
+    expect(getStreak(moods)).toBe(0);
+  });
+
+  test("returns 1 if last mood entered is positive", () => {
+    const moods = ["Happy"];
+    expect(getStreak(moods)).toBe(1);
+  });
 });
